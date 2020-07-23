10ed3a41779865415a731400a210e963c00c3562
SaitgalinAygiz <saitgalin18@gmail.com>
2020-07-23 17:14:33 +0500

    autoparts store page, citySelect modal window

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import React from 'react';
 import Routes from 'routes'
 import {BrowserRouter as Router} from "react-router-dom";
 import TopBar from 'components/topBar'
-import { ModalProvider } from 'contexts/modalContext'
+import {ModalProvider} from 'contexts/modalContext'
 import BottomBar from 'components/bottomBar'
+import {YMaps} from "react-yandex-maps";
 
 function App() {
     return (
         <div className="App">
-            <Router>
-                <ModalProvider>
-                    <TopBar/>
 
-                </ModalProvider>
-                <Routes/>
-            </Router>
-            <BottomBar/>
+            <YMaps
+                query={{apikey: 'e2345445-15e9-432b-8037-6107be685564'}}
+            >
+                <Router>
+                    <ModalProvider>
+                        <TopBar/>
+
+                    </ModalProvider>
+                    <Routes/>
+                </Router>
+                <BottomBar/>
+            </YMaps>
         </div>
     );
 }
